df65978f34b18d2b2216811ed08308da75aaf52e
koerriva <504097978@qq.com>
2020-08-06 22:43:24 +0800

    book

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import org.joml.Vector3f;
+
+public class Test {
+    public static void main(String[] args) {
+        Vector3f forward = new Vector3f(1,0,-1);
+        Vector3f up = new Vector3f(0,2,0);
+        Vector3f r = new Vector3f();
+        forward.cross(up,r);
+        System.out.println(r);
+        up.cross(forward,r);
+        System.out.println(r);
+    }
+}
